a837c643ec9dc5b2d68c845d74562efe4aa0f2d1
huanghu01 <1525273035@qq.com>
2024-09-23 02:06:49 +0800

    feat:引入图标

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import { ElementPlusContainer } from '@vitepress-demo-preview/component'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import '@vitepress-demo-preview/component/dist/style.css'
 import 'element-plus/dist/index.css'
@@ -12,6 +13,9 @@ export default {
   enhanceApp: async ({ app, router, siteData }) => {
     app.component('demo-preview', ElementPlusContainer)
     app.use(ElementPlus, { locale: zhCn })
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+        app.component(key, component)
+    }    
     app.use(ZbUiInstall)
   }
 }
