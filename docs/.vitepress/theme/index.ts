006cafcd7a541fe01b711e1e1d8fcf48653aa599
huanghu <huanghu@eastmoney.com>
2024-09-23 10:12:51 +0800

    feat:先打包文件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import { ElementPlusContainer } from '@vitepress-demo-preview/component'
 import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import '@vitepress-demo-preview/component/dist/style.css'
 import 'element-plus/dist/index.css'
@@ -14,9 +13,6 @@ export default {
   enhanceApp: async ({ app, router, siteData }) => {
     app.component('demo-preview', ElementPlusContainer)
     app.use(ElementPlus, { locale: zhCn })
-    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-        app.component(key, component)
-    }
     app.use(ZbUiInstall) 
   }
 }
