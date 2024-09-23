3f9570fd85be593b5403a48505e257535b54c18d
huanghu <huanghu@eastmoney.com>
2024-09-23 09:48:03 +0800

    feat:引用修改

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,6 +5,9 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import '@vitepress-demo-preview/component/dist/style.css'
 import 'element-plus/dist/index.css'
+import 'zb-vue3-ui/es/style.css'
+import { ZbUiInstall } from 'zb-vue3-ui'
+
 
 export default {
   ...DefaultTheme,
@@ -14,5 +17,6 @@ export default {
     for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
         app.component(key, component)
     }
+    app.use(ZbUiInstall) 
   }
 }
